903415c6adb695c42b283cad965aa604efdd9a2d
anand2312 <40204976+anand2312@users.noreply.github.com>
2023-05-15 16:41:06 +0530

    Make NFA evaluator
    This does not yet work with null transitions

---
+++ src/nfa.rs
@@ -0,0 +1,103 @@
+use crate::fa::{Acceptor, FAConfig, ReadFAConfig, State};
+use std::collections::{HashMap, HashSet};
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct NFA {
+    /// Evaluate and validate the DFA described by a given DFAConfig
+    states: HashMap<usize, State>,
+    initial_state: usize,
+    final_states: HashSet<usize>,
+    transitions: Vec<Vec<Vec<char>>>, // adjacency matrix for the state graph
+}
+
+impl ReadFAConfig for NFA {
+    fn from_file(path: PathBuf) -> Self {
+        let config = FAConfig::from_file(path);
+        Self::from_config(config)
+    }
+
+    fn from_config(config: FAConfig) -> Self {
+        let mut states = HashMap::new();
+        let mut state_to_idx: HashMap<String, usize> = HashMap::new();
+        let mut initial_state: usize = usize::MAX;
+
+        for (idx, name) in config.states.into_iter().enumerate() {
+            states.insert(
+                idx,
+                State {
+                    name: name.clone(),
+                    index: idx,
+                },
+            );
+            state_to_idx.insert(name.clone(), idx);
+            if name == config.initial_state {
+                initial_state = idx;
+            }
+        }
+
+        if initial_state == usize::MAX {
+            panic!("Error reading configuration: Initial state not present in set of all states");
+        }
+
+        let mut final_states = HashSet::new();
+        for name in config.final_states.into_iter() {
+            if let Some(s) = state_to_idx.get(&name) {
+                final_states.insert(s.to_owned());
+            } else {
+                panic!(
+                    "Error reading configuration: Final state {} not present in set of all states",
+                    name
+                );
+            }
+        }
+
+        let mut transitions = vec![vec![vec![]; states.len()]; states.len()];
+        for (idx, transition) in config.transitions.into_iter().enumerate() {
+            if !config.alphabet.contains(&transition.on) {
+                panic!("Error reading configuration: Character {} not present in given alphabet for DFA", transition.on);
+            } else {
+                let y = state_to_idx.get(&transition.from).expect(
+                    format!("Error reading configuration: State {} not found in set of all states while parsing transition {}", transition.from, idx + 1).as_str()
+                );
+                let x = state_to_idx.get(&transition.to).expect(
+                    format!("Error reading configuration: State {} not found in set of all states while parsing transition {}", transition.from, idx + 1).as_str()
+                );
+                transitions[*y][*x].push(transition.on.chars().next().expect("Error reading configuration: exhausted string iterator while parsing transitions"));
+            }
+        }
+
+        return NFA {
+            states,
+            initial_state,
+            final_states,
+            transitions,
+        };
+    }
+}
+
+impl Acceptor for NFA {
+    fn test_string(&self, s: String) -> bool {
+        let chars = s.chars().collect::<Vec<_>>();
+        backtrack(self, self.initial_state, 0, &chars)
+    }
+}
+
+fn backtrack(nfa: &NFA, current_state: usize, current_char: usize, chars: &Vec<char>) -> bool {
+    if current_char == chars.len() {
+        let a = nfa.final_states.contains(&current_state);
+        return a;
+    }
+    let mut result = false;
+    for (idx, neighborhood) in nfa.transitions[current_state].iter().enumerate() {
+        if neighborhood.is_empty() {
+            continue;
+        }
+        // TODO: this doesn't account for null transitions yet!
+        if neighborhood.contains(&chars[current_char]) {
+            // if ANY of the paths result in an acceptance, the string is accepted
+            result = result | backtrack(nfa, idx, current_char + 1, chars);
+        }
+    }
+    result
+}
