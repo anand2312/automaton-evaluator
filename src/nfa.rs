89577b5da92c3b41fbaad4ff4c6c9088b31b2947
anand2312 <40204976+anand2312@users.noreply.github.com>
2023-06-03 19:15:41 +0530

    Don't use backtracking for NFA evaluation

--- src/nfa.rs
+++ src/nfa.rs
@@ -99,12 +99,11 @@ impl ReadFAConfig for NFA {
 
 impl Acceptor for NFA {
     fn test_string(&self, s: String) -> bool {
-        let chars = s.chars().collect::<Vec<_>>();
-        backtrack(self, self.initial_state, 0, &chars)
+        acceptor(self, s)
     }
 }
 
-fn backtrack(nfa: &NFA, current_state: usize, current_char: usize, chars: &Vec<char>) -> bool {
+fn _backtrack(nfa: &NFA, current_state: usize, current_char: usize, chars: &Vec<char>) -> bool {
     if current_char == chars.len() {
         let a = nfa.final_states.contains(&current_state);
         return a;
@@ -121,12 +120,116 @@ fn backtrack(nfa: &NFA, current_state: usize, current_char: usize, chars: &Vec<c
         // consuming current character).
         if neighborhood.contains(&chars[current_char]) {
             // if ANY of the paths result in an acceptance, the string is accepted
-            result |= backtrack(nfa, idx, current_char + 1, chars);
+            result |= _backtrack(nfa, idx, current_char + 1, chars);
         }
         if neighborhood.contains(&'\u{03F5}') {
             // use the null transition to go to the next state without consuming a character
-            result |= backtrack(nfa, idx, current_char, chars);
+            result |= _backtrack(nfa, idx, current_char, chars);
         }
     }
     result
+}
+
+// Another implementation for NFA evaluation: keep track of the set of possible current states
+// after consuming each character.
+// 0. Find the null closure of the current state(s)
+// 1. When the NFA can go to multiple states on a single input, keep track of all of these states in a set
+// 2. Find the null closure of the set of states after step 0 and 1
+// After consuming all characters, if the set of current states and set of final states have non-empty
+// intersection, the string was accepted.
+// This approach should run in expected linear time, as opposed to the backtracking approach taking
+// possibly exponential time as the number of paths through the graph is 2^n
+// The backtracking approach only needs to be used if we needed the exact PATH taken through the graph.
+fn null_closure(nfa: &NFA, current_states: HashSet<usize>) -> HashSet<usize> {
+    let mut res = current_states.clone();
+    let mut old_length = current_states.len();
+    // println!("before finding closure = {:?}", &current_states);
+    for state in current_states {
+        for (idx, neighborhood) in nfa.transitions[state].iter().enumerate() {
+            if neighborhood.contains(&'\u{03F5}') {
+                res.insert(idx);
+            }
+        }
+    }
+
+    let mut new_length = res.len();
+    if old_length == new_length {
+        return res;
+    }
+    let mut next_closure = null_closure(nfa, res);
+    while new_length != old_length {
+        old_length = next_closure.len();
+        next_closure = null_closure(nfa, next_closure);
+        new_length = next_closure.len();
+    }
+    // println!("null closure = {:?}", &next_closure);
+    next_closure
+}
+
+fn current_states_after_consuming_char(nfa: &NFA, current_states: HashSet<usize>, current_char: char) -> HashSet<usize> {
+    let closure = null_closure(nfa, current_states);
+    let mut next_states = HashSet::new();
+    for state in closure {
+        for (idx, neighborhood) in nfa.transitions[state].iter().enumerate() {
+            if neighborhood.contains(&current_char) {
+                next_states.insert(idx);
+            }
+        }
+    }
+    null_closure(nfa, next_states)
+}
+
+fn acceptor(nfa: &NFA, s: String) -> bool {
+    let mut current_states = HashSet::new();
+    current_states.insert(nfa.initial_state);
+    // println!("initial state = {:?}", &current_states);
+    for character in s.chars() {
+        current_states = current_states_after_consuming_char(nfa, current_states, character);
+    }
+    !current_states.is_disjoint(&nfa.final_states) // if the sets are disjoint, no final states were reached
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use crate::{fa::ReadFAConfig, nfa::{_backtrack, acceptor}};
+    use super::NFA;
+
+    #[test]
+    fn test_nfa01() {
+        // Make sure the other approach for NFA evaluation gives the same results
+        // as the backtracking approach.
+        let mut path = PathBuf::from("./test_configs/");
+        // test nfa01.json - strings ending in ab
+        path.push("nfa01.json");
+        let nfa = NFA::from_file(path);
+        let test_cases = ["abbbab", "bab", "b", "", "bbbbbbbbbba"];
+        for input in test_cases {
+            assert_eq!(_backtrack(&nfa, nfa.initial_state, 0, &input.clone().chars().collect::<Vec<char>>()), acceptor(&nfa, input.to_owned()));
+        }
+    }
+
+    #[test]
+    fn test_nfa02() {
+        let mut path = PathBuf::from("./test_configs/");
+        // test nfa02.json - strings starting with a
+        path.push("nfa02.json");
+        let nfa = NFA::from_file(path);
+        let test_cases = ["abbbab", "bab", "b", "", "bbbbbbbbbba", "a"];
+        for input in test_cases {
+            assert_eq!(_backtrack(&nfa, nfa.initial_state, 0, &input.clone().chars().collect::<Vec<char>>()), acceptor(&nfa, input.to_owned()));
+        }
+    }
+
+    #[test]
+    fn test_nfa03() {
+        let mut path = PathBuf::from("./test_configs/");
+        // test nfa03.json - strings matching a regex b*a
+        path.push("nfa03.json");
+        let nfa = NFA::from_file(path);
+        let test_cases = ["abbbab", "bab", "b", "a", "bba", "bbbba"];
+        for input in test_cases {
+            assert_eq!(_backtrack(&nfa, nfa.initial_state, 0, &input.clone().chars().collect::<Vec<char>>()), acceptor(&nfa, input.to_owned()));
+        }
+    }
 }
